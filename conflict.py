67521d62b1270f0393340b133dd4b6261d80497e
Sophie Martens <s.g.m.m.martens@students.uu.nl>
2019-01-17 14:46:36 +0100

    now with a csv file

--- conflict.py
+++ conflict.py
@@ -1,9 +1,25 @@
 import json
-from pprint import pprint
+import csv
 
-with open('conflict_data_full_lined.json') as data_file:    
+with open('conflict_data_full_lined.json', encoding= 'utf8') as data_file:    
     conflict_raw = json.load(data_file)
     conflict_mexico = []
-    for observations in conflict_raw:
-        if observations['country']== "Mexico":
-	        conflict_mexico.append(observations)
+    for observation in conflict_raw:
+        if observation['country']== "Mexico":
+	        conflict_mexico.append(observation)
+
+header = []
+for observation in conflict_mexico:
+    for key in observation.keys():
+        if key not in header:
+            header.append(key)
+
+with open('conflict_mexico.csv', 'w') as file:
+    writer = csv.DictWriter(file, fieldnames=header, lineterminator='\n', delimiter=',')
+    
+    writer.writeheader()
+
+    for observation in conflict_mexico:
+        writer.writerow(observation)
+
+
